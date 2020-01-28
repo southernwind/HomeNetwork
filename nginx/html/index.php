366565e2b7c43f49a64ceda660516eee6b58d1e2
southernwind <testakina@gmail.com>
2020-01-28 16:28:24 +0900

    PowerBI家計簿へのリンクを追加

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -11,5 +11,8 @@
         <div>
             <a href="./phpinfo.php">PHP INFO</a>
         </div>
+        <div>
+            <a href="https://app.powerbi.com/groups/me/reports/93484b32-0a35-4af6-8b0e-c521647bf74b/ReportSection">家計</a>
+        </div>
     </body>
 </html>
