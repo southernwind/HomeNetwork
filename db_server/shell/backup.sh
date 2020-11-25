e050cdf8fd7d88e737426bd27fce71aac335e735
southerndind <testakina@gmail.com>
2020-11-25 23:17:01 +0900

    DBの月次バックアップ、年次バックアップを追加

--- db_server/shell/backup.sh
+++ db_server/shell/backup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "ERROR" 1>&2
     exit 1
 fi
 
 MYSQL_PASSWORD=$1
 RSYNC_HOST=$2
+FORMAT=$3
 
-DATE=`date +%d`
+DATE=`date +${FORMAT}`
 
 # dump
 mysqldump -uroot -p${MYSQL_PASSWORD} -h 127.0.0.1 -A --ignore-table=mysql.* --ignore-table=information_schema.* --ignore-table=performance_schema.* --extended-insert --single-transaction > /tmp/database.dump
