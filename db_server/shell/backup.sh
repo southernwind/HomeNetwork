c01787c66f59bc7c10d8ef4c9463c970c068a632
southerndind <testakina@gmail.com>
2020-11-29 11:14:56 +0900

    dbバックアップが動いてない

--- db_server/shell/backup.sh
+++ db_server/shell/backup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
     echo "ERROR" 1>&2
     exit 1
 fi
 
-MYSQL_PASSWORD=$1
-RSYNC_HOST=$2
-FORMAT=$3
+RSYNC_HOST=$1
+FORMAT=$2
 
 DATE=`date +${FORMAT}`
 
 # dump
-mysqldump -uroot -p${MYSQL_PASSWORD} -h 127.0.0.1 -A --ignore-table=mysql.* --ignore-table=information_schema.* --ignore-table=performance_schema.* --extended-insert --single-transaction > /tmp/database.dump
+mysqldump -uroot -h 127.0.0.1 -A --ignore-table=mysql.* --ignore-table=information_schema.* --ignore-table=performance_schema.* --extended-insert --single-transaction > /tmp/database.dump
 
 # compress
 tar -cjf /root/backup/database.dump-${DATE}.bz2 /tmp/database.dump --overwrite
